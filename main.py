3eef37baa92c0f5f9dd1f7a48c48190bba5c9619
Dimitar ivanov <divanov32@dxc.com>
2025-10-24 18:45:51 +0300

    added cors

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import json
 from datetime import datetime
+from flask_cors import CORS  # <-- import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def analyze_transactions(data):
     # Extract booked and pending
